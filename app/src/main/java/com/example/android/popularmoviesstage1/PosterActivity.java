36b6c4b1681fd52f6497d2b2f01719e5d947f245
Seth Watkins <seth.personal.smtp@gmail.com>
2017-07-30 09:00:54 -0500

    Implement PosterAdapter.ItemClickListener

--- app/src/main/java/com/example/android/popularmoviesstage1/PosterActivity.java
+++ app/src/main/java/com/example/android/popularmoviesstage1/PosterActivity.java
@@ -21,13 +21,15 @@ import com.example.android.popularmoviesstage1.utilities.NetworkUtils;
 import java.net.URL;
 import java.util.ArrayList;
 
-public class PosterActivity extends AppCompatActivity {
+public class PosterActivity extends AppCompatActivity implements PosterAdapter.ItemClickListener {
     public static final String LOG_TAG = PosterActivity.class.getSimpleName();
     RecyclerView posters;
     ProgressBar mProgressBar;
     String fetchUrl = NetworkUtils.MOST_POPULAR_QUERY;
     TextView mHeaderView;
     TextView mErrorView;
+    Toast errorToast;
+    ArrayList<Movie> movieData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,26 +38,38 @@ public class PosterActivity extends AppCompatActivity {
         mProgressBar = (ProgressBar) findViewById(R.id.loading_progress_bar);
         mHeaderView = (TextView) findViewById(R.id.header_text_view);
         mErrorView = (TextView) findViewById(R.id.error_text_view);
-
+        errorToast = Toast.makeText(this, R.string.error_msg, Toast.LENGTH_LONG);
         posters = (RecyclerView) findViewById(R.id.posters_recycler_view);
+
         posters.setLayoutManager(new GridLayoutManager(this, 3));
+
         if (NetworkUtils.hasInternet(this)) {
+            Log.i(LOG_TAG, "............We have internet");
+            Log.i(LOG_TAG, "FetchDataTask is about to execute");
             new FetchDataTask().execute();
         } else {
             mErrorView.setVisibility(View.VISIBLE);
+            mProgressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        if (movieData != null) {
+            Toast.makeText(this, "You just clicked " + position, Toast.LENGTH_SHORT).show();
         }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.order, menu);
+
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        Toast errorToast = Toast.makeText(this, R.string.error_msg, Toast.LENGTH_LONG);
 
         if (id == R.id.top_rated_item) {
             fetchUrl = NetworkUtils.HIGHEST_RATED_QUERY;
@@ -64,6 +78,7 @@ public class PosterActivity extends AppCompatActivity {
                 new FetchDataTask().execute();
             } else {
                 errorToast.show();
+                mProgressBar.setVisibility(View.INVISIBLE);
             }
             return true;
         } else if (id == R.id.most_popular_item) {
@@ -73,6 +88,7 @@ public class PosterActivity extends AppCompatActivity {
                 new FetchDataTask().execute();
             } else {
                 errorToast.show();
+                mProgressBar.setVisibility(View.INVISIBLE);
             }
             return true;
         }
@@ -83,22 +99,24 @@ public class PosterActivity extends AppCompatActivity {
     public class FetchDataTask extends AsyncTask<Void, Void, ArrayList<Movie>> {
         @Override
         protected ArrayList<Movie> doInBackground(Void... params) {
+            errorToast.cancel();
             URL url = NetworkUtils.urlFromString(fetchUrl);
             if (url != null) {
                 String responseString = NetworkUtils.getResponseFromURL(url);
                 Log.v(LOG_TAG, responseString);
                 return JSONUtils.parseMovies(responseString);
             }
-
             return null;
         }
 
         @Override
         protected void onPostExecute(ArrayList<Movie> movies) {
             mProgressBar.setVisibility(View.INVISIBLE);
-            if (movies.isEmpty()) {
+            if (movies == null || movies.isEmpty()) {
                 mErrorView.setVisibility(View.VISIBLE);
                 return;
+            } else {
+                mErrorView.setVisibility(View.INVISIBLE);
             }
             Log.i(LOG_TAG, "onPostExecute is running =)");
             for (int i = 0; i < movies.size(); i++) {
@@ -106,8 +124,16 @@ public class PosterActivity extends AppCompatActivity {
                 Log.i(LOG_TAG, movie.getTitle() + "\n" + movie.getThumbnailPath() + "\n" + movie.getPlotSynopsis() + "\n" + movie.getUserRating() + "\n" + movie.getReleaseDate());
             }
             PosterAdapter mAdapter = new PosterAdapter(PosterActivity.this, movies);
+            mAdapter.setOnClickListener(PosterActivity.this);
             posters.setAdapter(mAdapter);
+            movieData = movies;
 
         }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        errorToast.cancel();
     }
 }
